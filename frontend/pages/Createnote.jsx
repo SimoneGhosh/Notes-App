47ba151dda5053d6103baec49a9e21fa84307c8d
SimoneGhosh <simone.ghoshbd@gmail.com>
2025-09-29 22:55:35 -0400

    Add files via upload

---
+++ frontend/pages/Createnote.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Noteform from '../components/Noteform'
+
+const Createnote = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-gray-900'><Noteform/></div>
+  )
+}
+
+export default Createnote
